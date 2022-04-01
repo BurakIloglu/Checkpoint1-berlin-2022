2d7bd51b3970c92734a87f923ed5783ded55d7b8
hoolby <hoolby@users.noreply.github.com>
2022-04-01 10:02:02 +0300

    Initialize cabanat_axel

---
+++ algo/4/findAdults.js
@@ -0,0 +1,40 @@
+/*
+Voici un tableau de personnes.
+Tu dois écire le code de la fonction findAdult, qui renvoie un
+nouveau tableau avec 2 tableaux à l'intérieur: 
+- Le premier avec uniquement les femmes adultes.
+- Le deuxième avec uniquement les hommes adultes.
+(Compte tenu qu'on est adulte à 18 ans)
+Exemple : output [ [femmes adultes], [hommes adultes] ]
+*/
+const persons = [
+  { name: 'Mary Poppins', age: 32, sex: 'female' },
+  { name: 'Tony Stark', age: 36, sex: 'male' },
+  { name: 'John Doe', age: 12, sex: 'male' },
+  { name: 'Jane Doe', age: 6, sex: 'female' },
+  { name: 'Maggie Simpson', age: 2, sex: 'female' },
+  { name: 'Leonardo', age: 92, sex: 'male' },
+  { name: 'Carla Moreau', age: 24, sex: 'female' },
+  { name: 'Mickael Jackson', age: 17, sex: 'male' },
+  { name: 'Penelope Milena', age: 27, sex: 'female' },
+  { name: 'Homer Simpson', age: 45, sex: 'male' },
+  { name: 'Leonardo Di Caprio', age: 42, sex: 'male' },
+  { name: 'Carla Bruni', age: 54, sex: 'female' },
+  { name: 'Lisa Simpson', age: 13, sex: 'female' },
+  { name: 'Millie Bobby Brown', age: 15, sex: 'female' },
+  { name: 'Penelope Cruz', age: 47, sex: 'female' },
+];
+
+function findAdults(persons) {
+  const result = [[],[]];
+  for (let i = 0; i<persons.length; i++){
+    if (persons[i].age>=18 && persons[i].sex === 'female'){
+      result[0].push(persons[i]);
+    } else if (persons[i].age>=18 && persons[i].sex === 'male'){
+      result[1].push(persons[i]);
+    }
+  }
+  return result;
+}
+
+module.exports = findAdults;
---
+++ algo/6/sumArr.js
@@ -0,0 +1,40 @@
+/*
+écris une fonction, qui prend deux paramètres.
+Ces deux paramètres sont des arrays de string numbers.
+Ta fonction doit renvoyer un seul array avec la somme des
+éléments correspondants en string.
+Si l'un des deux éléments est vide, il doit être considéré comme 0.
+Exemple :
+sumArr( ["1", "2", "3"], ["2", "4", "1"] ) doit renvoyer ["3", "6", "4"]
+sumArr( ["2", "7", "3"], ["2", "4", "9"] ) doit renvoyer ["4", "11", "12"]
+sumArr( ["2", "7", "3", "8", "2"], ["2", "4", "9"] ) doit renvoyer ["4", "11", "12", "8", "2"]
+*/
+
+function sumArr(arrayA, arrayB) {
+  const result = [];
+  let numberA
+  let numberB
+  for (i=0 ; i< arrayB.length; i++){
+    if (arrayA[i]=== null || arrayA[i]=== "0" ){
+      numberA = 0
+    } else {
+      numberA = parseInt(arrayA[i]);
+    }
+    if (arrayB[i]=== null || arrayB[i]=== "0" ){
+      numberB = 0
+    } else {
+      numberB = parseInt(arrayB[i]);
+    }
+    result.push((numberA+numberB).toString())
+    
+  }
+  if (arrayA.length>arrayB.length){
+    for (i= arrayB.length; i< arrayA.length; i++){
+      result.push(arrayA[i])
+    }
+  }
+  
+  return result
+}
+
+module.exports = sumArr;
