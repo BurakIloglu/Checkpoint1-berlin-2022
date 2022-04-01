1e18053e294ad09986b98a26e0c9f3a3d0ed80d0
hoolby <hoolby@users.noreply.github.com>
2022-04-01 10:02:15 +0300

    Initialize narboni_jonathan

---
+++ algo/3/theaterSieges.js
@@ -0,0 +1,35 @@
+/*
+Un employé de théatre souhaiterait obtenir la liste de tous les sièges présents dans sa salle principale.
+Dans la salle les sièges sont répartis de la manière suivantes : 
+  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
+  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+
+Compléter la fonction theaterSieges de manière à ce qu'elle retourne une matrice 
+où chaque sous-tableau liste les positions des sieges d'une rangée.
+
+exemple :
+
+[
+  ['1-1', '1-2', '1-3', ..., '1-100'], 
+  ['2-1', '2-2', '2-3', ..., '2-100'],
+  ... 
+  ['26-1', '26-2', '26-3', ..., '26-100']
+] 
+*/
+
+function theaterSieges() {
+  // Your code here !
+  const bordel = [];
+  for (i = 0 ; i < 26 ; i++) {
+    let array = [];
+    for (x = 0 ; x < 100; x++ ){
+      array.push(`${i+1}-${x+1}`);
+    }
+    bordel.push(array);
+  }
+  return bordel
+}
+
+console.log(theaterSieges());
+
+module.exports = theaterSieges;
