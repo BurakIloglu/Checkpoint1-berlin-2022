a71980a5c41135f50928addc8212dddf7365ab43
hoolby <hoolby@users.noreply.github.com>
2022-04-01 10:01:56 +0300

    Initialize add-jest

---
+++ algo/algo4.test.js
@@ -0,0 +1,29 @@
+const findAdults = require("./algo4");
+
+const persons = [
+  { name: "Mary Poppins", age: 32, sex: "female" },
+  { name: "Tony Stark", age: 36, sex: "male" },
+  { name: "John Doe", age: 12, sex: "male" },
+  { name: "Jane Doe", age: 6, sex: "female" },
+  { name: "Homer Simpson", age: 45, sex: "male" },
+  { name: "Leonardo Di Caprio", age: 42, sex: "male" },
+  { name: "Carla Bruni", age: 54, sex: "female" },
+  { name: "Lisa Simpson", age: 13, sex: "female" },
+  { name: "Millie Bobby Brown", age: 15, sex: "female" },
+  { name: "Penelope Cruz", age: 47, sex: "female" },
+];
+
+const men = [
+  { name: "Tony Stark", age: 36, sex: "male" },
+  { name: "Homer Simpson", age: 45, sex: "male" },
+  { name: "Leonardo Di Caprio", age: 42, sex: "male" },
+];
+const women = [
+  { name: "Mary Poppins", age: 32, sex: "female" },
+  { name: "Carla Bruni", age: 54, sex: "female" },
+  { name: "Penelope Cruz", age: 47, sex: "female" },
+];
+
+test("should return an array with two array of major men and women", () => {
+  expect(findAdults(persons)).toEqual([women, men]);
+});
