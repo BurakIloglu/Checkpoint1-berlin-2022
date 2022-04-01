58cf5159c3d14c852d950bffee8711ecca0fd002
hoolby <hoolby@users.noreply.github.com>
2022-04-01 10:02:21 +0300

    Initialize angeliquerioux

---
+++ algo/4/findAdults.js
@@ -0,0 +1,50 @@
+/*
+Voici un tableau de personnes.
+Tu dois écire le code de la fonction findAdult, qui renvoie un
+nouveau tableau avec 2 tableaux à l'intérieur: 
+- Le premier avec uniquement les femmes adultes.
+- Le deuxième avec uniquement les hommes adultes.
+(Compte tenu qu'on est adulte à 18 ans)
+Exemple : output [ [femmes adultes], [hommes adultes] ]
+*/
+const persons = [
+  { name: 'Mary Poppins', age: 32, sex: 'female' },
+  { name: 'Tony Stark', age: 36, sex: 'male' },
+  { name: 'John Doe', age: 12, sex: 'male' },
+  { name: 'Jane Doe', age: 6, sex: 'female' },
+  { name: 'Maggie Simpson', age: 2, sex: 'female' },
+  { name: 'Leonardo', age: 92, sex: 'male' },
+  { name: 'Carla Moreau', age: 24, sex: 'female' },
+  { name: 'Mickael Jackson', age: 17, sex: 'male' },
+  { name: 'Penelope Milena', age: 27, sex: 'female' },
+  { name: 'Homer Simpson', age: 45, sex: 'male' },
+  { name: 'Leonardo Di Caprio', age: 42, sex: 'male' },
+  { name: 'Carla Bruni', age: 54, sex: 'female' },
+  { name: 'Lisa Simpson', age: 13, sex: 'female' },
+  { name: 'Millie Bobby Brown', age: 15, sex: 'female' },
+  { name: 'Penelope Cruz', age: 47, sex: 'female' },
+];
+
+function findAdults(arr) {
+  // Your code here !
+  let woman = 0
+  let man = 0
+  let arrWoman = []
+  let arrMan = []
+  let array = []
+  for (let i = 0; i < arr.length; i++) {
+      if (persons[i].age >= 18 && persons[i].sex === 'female') {
+          arrWoman[woman] = persons[i]
+          woman += 1
+      }
+      else if (persons[i].age >= 18 && persons[i].sex === 'male') {
+          arrMan[man] = persons[i]
+          man += 1
+      }
+  }
+  array.push(arrWoman)
+  array.push(arrMan)
+  return array
+}
+
+module.exports = findAdults;
---
+++ algo/6/sumArr.js
@@ -0,0 +1,26 @@
+/*
+écris une fonction, qui prend deux paramètres.
+Ces deux paramètres sont des arrays de string numbers.
+Ta fonction doit renvoyer un seul array avec la somme des
+éléments correspondants en string.
+Si l'un des deux éléments est vide, il doit être considéré comme 0.
+Exemple :
+sumArr( ["1", "2", "3"], ["2", "4", "1"] ) doit renvoyer ["3", "6", "4"]
+sumArr( ["2", "7", "3"], ["2", "4", "9"] ) doit renvoyer ["4", "11", "12"]
+sumArr( ["2", "7", "3", "8", "2"], ["2", "4", "9"] ) doit renvoyer ["4", "11", "12", "8", "2"]
+*/
+
+function sumArr(arrayA, arrayB) {
+  // Your code here !
+  const sum = [];
+  arrayA.forEach((element, index) => {
+    if (index <= (arrayB.length - 1)) {
+      sum.push((parseFloat(element) + parseFloat(arrayB[index])).toString());
+    }
+    else {
+      sum.push(element);
+    }
+  })
+  return sum;
+}
+module.exports = sumArr;
